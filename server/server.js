52df770d201bad13ce6f8bffc7491b9c985841cf
Eric Pratt <ericpratt86@gmail.com>
2018-05-19 16:48:33 -0700

    typo fix

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 const app = express();
-const port = process.end.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
