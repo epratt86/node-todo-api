ad518d12e078f6cdd33d21e67ac4672f0dcf0292
Eric Pratt <ericpratt86@gmail.com>
2018-05-29 12:31:23 -0700

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = '123abc!';
+
+//this creates a hashed password 
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+let hashedPassword = '$2a$10$5qkGzcL0FxGJtGsJRv3Y3uot7ytVZR9PPzryIQIqAZmCReIjui99y';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// let data = {
+//   id: 10
+// };
 
 //second arg is the secret code that hashes data
-let token = jwt.sign(data, '123abc');
-console.log(token);
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 
 // let message = 'I am a user';
 // let hash = SHA256(message).toString();
